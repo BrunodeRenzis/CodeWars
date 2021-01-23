be53985631c5b204afe1af97ddb58ddad171dd87
Bruno de Renzis <brunoderenzisrubinetti26@gmail.com>
2021-01-23 16:08:12 -0300

    Ejercicios codewars

---
+++ Take a Ten Minute Walk/Take a Ten Minute Walk/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Take_a_Ten_Minute_Walk
+{
+    public class Kata
+    {
+        public static bool IsValidWalk(string[] walk)
+        {
+            if (walk.Length != 10)
+            {
+                return false;
+            }
+            else
+            {
+                foreach (string letra in walk)
+                {
+                    if (walk[0] == walk[2] && walk[1] == walk[3])
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+}
